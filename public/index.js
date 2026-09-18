ec7fec65c0d2f6588ff9b426fa6d5b7f2b549719
Exequiel <romeromatiasexequiel@hotmail.com>
2026-09-18 18:35:18 -0300

    feat: add dotenv for environment variables and implement usuarios routes

--- public/index.js
+++ public/index.js
@@ -1,12 +1,21 @@
 console.log("Hola Mundo desde js");
+const brn = document.getElementById("btn");
+const box = document.getElementById("box");
 
 const peticionPrincipal = async () => {
 
     const response = await fetch('http://localhost:3000/');
     const data = await response.json();
 
+    console.log("dataPrueba");
+
     console.log(data);
-    
+
+    data.forEach((usuario) => {
+        box.innerHTML += `<p>${usuario.nombre}</p>
+        <p>${usuario.email}</p>`;
+    });
+
 }
 
 peticionPrincipal();
--- src/app.js
+++ src/app.js
@@ -1,27 +1,47 @@
 import express from 'express';
 import cors from 'cors';
 import connection from './database/connection.js';
+import usuariosRoutes from './routes/usuarios.routes.js'
+import 'dotenv/config';
 
 
 const app = express();
+app.use(cors());
 
-app.use(cors()); 
+const variable = process.env.VALOR_DATO;
+console.log(variable);
+const PORT = process.env.PORT || 3000;
+
+app.use('/usuarios', ruterUsuario);
+
+
 // app.use(express.json());
 
 // const [rows] = await connection.query(
 //     "SELECT * FROM usuarios"
 // );
-
-// console.log(rows);
+//  const rows = 
+//      {
+//          id: 1,
+//          nombre: "Juan",
+//          email: "corre@corres"
+//      }
+//  console.log(rows);
 
 
 app.get('/', async (req, res) => {
     const [rows] = await connection.query(
         "SELECT * FROM usuarios"
     );
+    // const [rows] = 
+    // {
+    //     id: 1,
+    //     nombre: "Juan",
+    //     email: "corre@corres"
+    // }
     res.json(rows);
 });
 
-app.listen(3000, () => {
-    console.log(`Servidor corriendo en https://localhost:3000`);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en https://localhost:${PORT}}`);
 })
